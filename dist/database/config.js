dca50124e31b5797f7a2ddc121032f51e7807a2b
sanjaydambal <sanjaydambal@gmail.com>
2024-05-15 10:58:10 +0530

    created the typescript configuration

---
+++ dist/database/config.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const pg_1 = __importDefault(require("pg"));
+const { Pool } = pg_1.default;
+const dotenv_1 = require("dotenv");
+(0, dotenv_1.config)();
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: 5432,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+exports.default = pool;
